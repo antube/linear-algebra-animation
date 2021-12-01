b91417261cee44dda111e370afe6608f10f456b1
Andrew Brown <andrew@mkbrown.org>
2021-12-01 15:34:24 -0500

    Began writing the third scene function

--- third_scene.m
+++ third_scene.m
@@ -1,1 +1,17 @@
-function  [failureFlag]third_scene(characterX, characterY)
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%   New Function %%%%%%%%%%%%%%%%%%%%%%%%%%%
+% third_scene
+% Parameters
+%       characterCenter: A vector representing the center of the character
+%
+% Outputs
+%       failureFlag: A flag which when set will indicate the running of the method failed
+%       characterCenter: A vector representing the center of the charcter after the third scene is complete
+
+function  [failureFlag, characterCenter] = third_scene(characterCenter)
+    % character falls into scene from the previous building jump
+    % character lands
+    % character throws ninja stars at target
+    % charcter then runs off scene
+
+    
