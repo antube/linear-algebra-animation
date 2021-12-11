f61fa450d4592c1b0224aa3a47ec5f538ca874bd
Stephen Horn <sbh12@wildcats.unh.edu>
2021-12-11 02:04:55 -0500

    function teleportTo() exact coords

--- sceneTwo.m
+++ sceneTwo.m
@@ -5,35 +5,24 @@
 
 
 %% Create background image
-clf %This clears the figure, so remove this line if you want to preserve a plot you have already made
-% This creates the 'background' axes
+clf %
 ha = axes('units','normalized', 'position',[0 0 1 1]);
-% Move the background axes to the bottom
 uistack(ha,'bottom');
-% Load in a background image and display it using the correct colors
-% The image used below, is just a Roadrunner scene I downloaded.
 I=imread('NinjaHome.jpg');
 hi = imagesc(I);
 colormap gray;
-% Turn the handlevisibility off so that we don't inadvertently plot into the axes again
-% Also, make the axes invisible
 set(ha,'handlevisibility','off', 'visible','off')
-% Now we can use the figure, as required.
-% For example, we can put a plot in an axes
-%axes('position',[0.3,0.35,0.4,0.4])
-
 filename = 'NinjaSword1.jpg';
 ninjaColor =[1, 1, 1];
-thresh = 215;
+thresh = 219;
 ninjasword1 = imread(filename);
 ns1mtx = fJpeg2pointsConverter(ninjasword1, thresh);
 [m,n]=size(ns1mtx);
 fprintf("%s size (thresh=%i) , [%i,%i]",filename,thresh,m,n);
 disp(m);  disp(n); 
-ns1mtx = [ns1mtx;ones(1,15851)];
+ns1mtx = [ns1mtx;ones(1,n)];
 S = [0.025 0 0; 0 0.025 0; 0 0 1];  %This is my rescaling matrix to shrink the character to fit the background
 ns1mtx = S*ns1mtx;
-%ns1mtx = ns1mtx - [0;20]*ones(1,n);
 ns1mtx_orig = ns1mtx;
 %% Play background music throughout all scenes.
 % Time the animation and match the length of animation with length of audio
@@ -41,61 +30,37 @@ ns1mtx_orig = ns1mtx;
 player = audioplayer(y,Fs);
 play(player)   % Start the player
 stop(player)   % Stop whenever you like...
-%% Notes from Stephen
-%{ 
-(sbh)
-This "hb" thing is used for clipping the ninja to a specified axis 
-rectangle smaller than the background.
-If you don't wan't clipping, just make this box line up with the 
-background.   To see the box, change boxVisible='on';  
-%}
-axesVisible = 'off'; 
+
+axesVisible = 'on'; 
 axesXpos = 0;
 axesYpos = 0;
 axesXdim = 1;
 axesYdim = 1; 
 
-%% Run towards the center of the building (using shear)
-ns1mtx = ShearHScene(ns1mtx,0.5);
-for i=1:0.25:4.5
-    hb = axes('units','normalized', 'position',[-0.2 .0625 1 1]);
-    %hb = axes('position',[axesXpos axesYpos axesXdim axesYdim]);
+
+
+for i=1:5
+    hb = axes('units','normalized', 'position',[-0.2 .0625 1.2 1]);
     h_rr = plot(hb,ns1mtx(1,:), ns1mtx(2,:),   '.', 'color', ninjaColor, 'MarkerSize', 1); 
-    axis([0 70 0 70]) %This let me set the scale I wanted in the inserted axes
+    axis([0 70 0 70]) ;
     set(gca,'color','none','handlevisibility',axesVisible,'visible',axesVisible)
     
+    nS = [1 0 0.5 ; 0 1 -0.5; 0 0 1 ];
+    ns1mtx = nS*ns1mtx;
     
-    Shift = [1 0 i; 0 1 0; 0 0 1];
-    ns1mtx = Shift*ns1mtx;
-    
-    
-    pause(0.4)
-    set(h_rr,'Visible','off')  % This line erases the image of the Road Runner and Wile E. Coyote
-    axis([0 70 0 70]) % This let me set the scale I wanted in the inserted axes
-    set( gca, 'color','none','handlevisibility','off','visible','off')
+    pause(0.1);
+    set(h_rr,'Visible','off');  
+    axis([0 70 0 70]) ;
+    set( gca, 'color','none','handlevisibility','off','visible','off');
 end
 
-%% Parkour
-ns1mtx = ShearHScene(ns1mtx,-0.5);
-ns1mtx = ReflHScene(ns1mtx);
-for i=1:0.5:10
-    hb = axes('units','normalized', 'position',[-0.2 .0625 1 1]);
-    %hb = axes('position',[axesXpos axesYpos axesXdim axesYdim]);
-    h_rr = plot(hb,ns1mtx(1,:), ns1mtx(2,:),   '.', 'color', ninjaColor, 'MarkerSize', 1); 
-    axis([0 70 0 70]) %This let me set the scale I wanted in the inserted axes
-    set(gca,'color','none','handlevisibility',axesVisible,'visible',axesVisible)
-    
-    
-    Shift = [1 0 i; 0 1 0; 0 0 1];
-    ns1mtx = Shift*ns1mtx;
-    
-    
-    pause(0.25)
-    set(h_rr,'Visible','off')  % This line erases the image of the Road Runner and Wile E. Coyote
-    axis([0 70 0 70]) % This let me set the scale I wanted in the inserted axes
-    set( gca, 'color','none','handlevisibility','off','visible','off')
-end
 
+% button =1;
+% while sum(button) <= 3
+%     [mx,my,button] = ginput(3);
+%     disp(mx);
+%     disp(my);
+% end
 
 
 disp('script completed');
@@ -105,6 +70,15 @@ disp('script completed');
 ----------------------------------------------------------
 Functions below
 %}
+
+function PPt = teleportTo(PP,tx,ty)
+    nc = centerPivot(ns1mtx); 
+    nP = [1 0 -1*nc(1) ; 0 1 -1*nc(2); 0 0 1 ];
+    ns1mtx = nP*ns1mtx;
+    nS = [1 0 tx ; 0 1 ty; 0 0 1 ];
+    ns1mtx = nS*ns1mtx;
+end
+
 
 function fpiv = feetPivot(PP)
     % Get a pivot point at the feet of the character.
@@ -176,6 +150,22 @@ function PPshsc = ShiftScene(PP,xD,yD)
     end
 end
 
+
+function PPrefl = ReflHScene(PP)
+    [Mrows Ncols] = size(PP);
+    if Mrows == 2,
+        RE = [-1 0 ; 0 1];
+    else ,
+        RE = [-1 0 0; 0 1 0; 0 0 1];
+    end
+    center = feetPivot(PP);
+    PPz = ShiftScene(PP, -1.0*center(1,1), -1.0*center(2,1));
+    if Mrows == 3,
+        PPrefl = (RE*PPz) + center;
+    else ,
+        PPrefl = (RE*PPz) + center(1:2 , :); 
+    end
+end
 
 function PPout = fJpeg2pointsConverter(BB,THRESHOLD)
     BB1=BB(:,:,1);
