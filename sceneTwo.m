5d230c689e204bc1be8182b2df30936ae1fe8be0
Stephen Horn <sbh12@wildcats.unh.edu>
2021-12-11 01:07:11 -0500

    match ninja scale

--- sceneTwo.m
+++ sceneTwo.m
@@ -3,7 +3,9 @@
 
 %}
 
-clf  %This clears the figure, so remove this line if you want to preserve a plot you have already made
+
+%% Create background image
+clf %This clears the figure, so remove this line if you want to preserve a plot you have already made
 % This creates the 'background' axes
 ha = axes('units','normalized', 'position',[0 0 1 1]);
 % Move the background axes to the bottom
@@ -21,41 +23,71 @@ set(ha,'handlevisibility','off', 'visible','off')
 %axes('position',[0.3,0.35,0.4,0.4])
 
 filename = 'NinjaSword1.jpg';
-ninjaColor =[1.0, 1, 1];
+ninjaColor =[1, 1, 1];
 thresh = 215;
 ninjasword1 = imread(filename);
 ns1mtx = fJpeg2pointsConverter(ninjasword1, thresh);
 [m,n]=size(ns1mtx);
 fprintf("%s size (thresh=%i) , [%i,%i]",filename,thresh,m,n);
 disp(m);  disp(n); 
-ns1mtx = 0.075*ns1mtx;
-ns1mtx = ns1mtx - [0;20]*ones(1,n);
+ns1mtx = [ns1mtx;ones(1,15851)];
+S = [0.025 0 0; 0 0.025 0; 0 0 1];  %This is my rescaling matrix to shrink the character to fit the background
+ns1mtx = S*ns1mtx;
+%ns1mtx = ns1mtx - [0;20]*ones(1,n);
 ns1mtx_orig = ns1mtx;
-
+%% Play background music throughout all scenes.
+% Time the animation and match the length of animation with length of audio
+[y,Fs] = audioread('ninja_music.wav');
+player = audioplayer(y,Fs);
+play(player)   % Start the player
+stop(player)   % Stop whenever you like...
+%% Notes from Stephen
 %{ 
 (sbh)
 This "hb" thing is used for clipping the ninja to a specified axis 
 rectangle smaller than the background.
 If you don't wan't clipping, just make this box line up with the 
-background.   To see the box, change axesVisible='on';  
+background.   To see the box, change boxVisible='on';  
 %}
-axesVisible = 'on'; 
-axesXpos = 0.1;
-axesYpos = 0.4;
-axesXdim = 0.625;
-axesYdim = 0.625; 
+axesVisible = 'off'; 
+axesXpos = 0;
+axesYpos = 0;
+axesXdim = 1;
+axesYdim = 1; 
 
-
-for ii=1:0.5:3
-    hb = axes('units','normalized', 'position',[0.1 0.4 0.625 0.625]);
+%% Run towards the center of the building (using shear)
+ns1mtx = ShearHScene(ns1mtx,0.5);
+for i=1:0.25:4.5
+    hb = axes('units','normalized', 'position',[-0.2 .0625 1 1]);
     %hb = axes('position',[axesXpos axesYpos axesXdim axesYdim]);
     h_rr = plot(hb,ns1mtx(1,:), ns1mtx(2,:),   '.', 'color', ninjaColor, 'MarkerSize', 1); 
     axis([0 70 0 70]) %This let me set the scale I wanted in the inserted axes
     set(gca,'color','none','handlevisibility',axesVisible,'visible',axesVisible)
     
     
-    Shift = [2.5*ii;ii]*ones(1,n);
-    ns1mtx = ns1mtx + Shift;
+    Shift = [1 0 i; 0 1 0; 0 0 1];
+    ns1mtx = Shift*ns1mtx;
+    
+    
+    pause(0.4)
+    set(h_rr,'Visible','off')  % This line erases the image of the Road Runner and Wile E. Coyote
+    axis([0 70 0 70]) % This let me set the scale I wanted in the inserted axes
+    set( gca, 'color','none','handlevisibility','off','visible','off')
+end
+
+%% Parkour
+ns1mtx = ShearHScene(ns1mtx,-0.5);
+ns1mtx = ReflHScene(ns1mtx);
+for i=1:0.5:10
+    hb = axes('units','normalized', 'position',[-0.2 .0625 1 1]);
+    %hb = axes('position',[axesXpos axesYpos axesXdim axesYdim]);
+    h_rr = plot(hb,ns1mtx(1,:), ns1mtx(2,:),   '.', 'color', ninjaColor, 'MarkerSize', 1); 
+    axis([0 70 0 70]) %This let me set the scale I wanted in the inserted axes
+    set(gca,'color','none','handlevisibility',axesVisible,'visible',axesVisible)
+    
+    
+    Shift = [1 0 i; 0 1 0; 0 0 1];
+    ns1mtx = Shift*ns1mtx;
     
     
     pause(0.25)
@@ -64,13 +96,6 @@ for ii=1:0.5:3
     set( gca, 'color','none','handlevisibility','off','visible','off')
 end
 
-hb = axes('units','normalized', 'position',[0.1 0.4 0.625 0.625]);
-h_rr = plot(hb, [1] , [1] ,   'yo',  'MarkerSize', 5); 
-axis([0 70 0 70]) %This let me set the scale I wanted in the inserted axes
-set(gca,'color','none','handlevisibility',axesVisible,'visible',axesVisible)
-Shift = [2.5*ii;ii]*ones(1,n);
-ns1mtx = ns1mtx + Shift;
-pause()
 
 
 disp('script completed');
@@ -153,18 +178,10 @@ end
 
 
 function PPout = fJpeg2pointsConverter(BB,THRESHOLD)
-%This function will take in an N x M x 3 matrix that 
-% has been imported into the workspace using the 
-% imread('filename.jpg') command and stored in a matrix
-% - it is called BB inside this converter.  
-%USAGE: BBout = Jpeg2pointsConverter(BB,THRESHOLD)
-
     BB1=BB(:,:,1);
     [M, N]= size(BB1);
     BB1=double(BB1);
-    BB2 = 255-BB1; %Invert so white is 0 instead of 255
-    %Any point with high value is replaced by 1, and 
-    %any point with a low value is replaced by 0
+    BB2 = 255-BB1; 
     BB3 = (BB2 > THRESHOLD);                     
     PP=zeros(2,M*N);
     cnt=0;
@@ -176,7 +193,6 @@ function PPout = fJpeg2pointsConverter(BB,THRESHOLD)
             end,
         end,
     end
-
     PPout = PP(:,1:cnt);
 end
 
